54d27018601bfa3583559bf79db11e4a8867f284
Tanmesh <tanmeshnm@gmail.com>
2024-08-25 16:03:55 -0700

    using queryKey

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,14 +1,20 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-const fetchSuperHero = async (id) => {
-    return await axios.get(`http://localhost:3004/superheroes?id=${id}`);
+/*
+* The function receives various values, out of which we use queryKey.
+* queryKey mimic the query array we have passed in useQuery (eg,  ["superhero", heroId])
+*/ 
+const fetchSuperHero = async ({ queryKey }) => {
+    const heroId = queryKey[1]
+    return await axios.get(`http://localhost:3004/superheroes?id=${heroId}`);
 }
 
 export const useSuperHeroData = (heroId, onSuccess, onError) => {
     return useQuery(
         ["superhero", heroId],
-        () => fetchSuperHero(heroId),
+        /* React Query automatically passes the parameters in the fetcher functions.  */
+        fetchSuperHero,
         {
             onSuccess: onSuccess,
             onError: onError,
