758a081f6fd58bc1abbec891d3e21b18c229997f
Tanmesh <tanmeshnm@gmail.com>
2024-08-25 17:45:36 -0700

    using Paginated Queries

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'; /* Importing Bootstrap CSS */
 import ParallelQueriesPage from "./components/ParallelQueries.page";
 import DynamicParallelQueriesPage from "./components/DynamicParallelQueries.page";
 import DependedQueriesPage from "./components/DependedQueries.page";
+import PaginatedQueriesPage from "./components/PaginatedQueries.page";
 
 const queryClient = new QueryClient();
 
@@ -40,6 +41,9 @@ function App() {
               <li>
                 <Link to="/rq-dependent-queries">Dependent Queries</Link>
               </li>
+              <li>
+                <Link to="/rq-paginated-queries">Paginated Queries</Link>
+              </li>
             </ul>
           </nav>
           <Routes>
@@ -50,6 +54,7 @@ function App() {
             {/* Lets mimic a scenario, where the User is passing dynamic list of heroIds */}
             <Route path="/rq-dynamic-parallel-queries" element={<DynamicParallelQueriesPage heroIds={[1, 3]} />} />
             <Route path="/rq-dependent-queries" element={<DependedQueriesPage email='tanmesh@example.com' />} />
+            <Route path="/rq-paginated-queries" element={<PaginatedQueriesPage />} />
             <Route path="/" element={<HomePage />} />
           </Routes>
         </div>
---
+++ src/components/PaginatedQueries.page.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { Button } from 'react-bootstrap'
+import { ArrowRight, ArrowLeft } from 'react-bootstrap-icons';
+
+
+const fetchColors = async (pageNumber) => {
+    return await axios.get(`http://localhost:3004/colors?_per_page=2&_page=${pageNumber}`);
+}
+
+const PaginatedQueriesPage = () => {
+    const [pageNumber, setPageNumber] = useState(1)
+    const { data, isLoading, isError, error, isFetching } = useQuery(
+        ['colors', pageNumber],
+        () => fetchColors(pageNumber),
+        {
+            select: (data) => data?.data?.data,
+            // This will maintain the last fetched data while fetching the new data.
+            keepPreviousData: true,
+        }
+    )
+
+    console.log(data)
+    return (
+        <>
+            <h2>Paginated Queries Page</h2>
+            {isLoading && <div>Loading...</div>}
+            {isFetching && <div>Fetching...</div>}
+            {isError && <div>Error: {error}</div>}
+            {data?.map(color =>
+                <div key={color.id}>
+                    {color.id}. {color.label}
+                </div>
+            )}
+            <div>
+                <Button
+                    variant="primary"
+                    size="sm"
+                    onClick={() => setPageNumber(pageNumber - 1)}
+                    disabled={pageNumber === 1}
+                >
+                    <ArrowLeft />
+                    Previous page
+                </Button>
+                <Button
+                    variant="primary"
+                    size="sm"
+                    onClick={() => setPageNumber(pageNumber + 1)}
+                    disabled={pageNumber === 4}
+                >
+                    Next page
+                    <ArrowRight />
+                </Button>
+            </div>
+        </>
+    )
+}
+
+export default PaginatedQueriesPage
