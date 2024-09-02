375b437f923a716d765fef36dc2a4123b3ebfc0f
Tanmesh <tanmeshnm@gmail.com>
2024-09-02 11:50:54 -0700

    using useAsyncDebounce

--- src/components/reactTable/GlobalFilter.js
+++ src/components/reactTable/GlobalFilter.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAsyncDebounce } from 'react-table'
 
 /**
  * This component is used to filter the data in the table.
  * When we need to use Client-side filtering, ie, when the data is already loaded in the client side, we can use this component.
  */
 const GlobalFilter = ({ filter, setFilter }) => {
+    const [value, setValue] = useState(filter)
+
+    const onChange = useAsyncDebounce(value => {
+        setFilter(value || undefined)
+    }, 1000)
+
     return (
         <span>
             Search: {' '}
             <input
-                value={filter || ''}
-                onChange={(e) => setFilter(e.target.value)}
+                value={value || ''}
+                onChange={(e) => {
+                    setValue(e.target.value)
+                    onChange(e.target.value)
+                }}
             />
         </span>
     )
