760e888dea1ca4b1cdae513495e1d70cea15a728
Tanmesh <tanmeshnm@gmail.com>
2024-09-01 18:23:04 -0700

    setting up useSelector

--- src/components/cakeShop/features/cake/CakeView.jsx
+++ src/components/cakeShop/features/cake/CakeView.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { StyledButton } from '../../../../styled-components/Button'
+import { useSelector } from 'react-redux'
 
 const CakeView = () => {
+    /**
+     * A selector function what takes redux state as an argument and returns the data that the component needs from the state.
+     */
+    const numOfCakes = useSelector((state) => state.cake.numOfCakes)
+
     return (
         <div>
-            <h4>Number of cakes - </h4>
+            <h4>Number of cakes - {numOfCakes}</h4>
             <StyledButton>Order cake</StyledButton>
             <StyledButton>Restock cakes</StyledButton>
         </div>
--- src/components/cakeShop/features/icecream/IcecreamView.jsx
+++ src/components/cakeShop/features/icecream/IcecreamView.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { StyledButton } from '../../../../styled-components/Button'
+import { useSelector } from 'react-redux';
 
 const IcecreamView = () => {
+    const numOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams)
+
     return (
         <div>
-            <h4>Number of ice-cream - </h4>
+            <h4>Number of ice-cream - {numOfIcecreams}</h4>
             <StyledButton>Order ice-cream</StyledButton>
             <StyledButton>Restock ice-cream</StyledButton>
         </div>
