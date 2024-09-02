fc7ad3ed4d685f2e6dca10fa4825a54c3f43dff6
Tanmesh <tanmeshnm@gmail.com>
2024-09-02 10:52:04 -0700

    reset

--- src/components/reactTable/ReactTable.page.js
+++ src/components/reactTable/ReactTable.page.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from 'react'
 import { useTable } from 'react-table'
 import MOCK_DATA from './MOCK_DATA.json'
-import { GROUPED_COLUMNS } from './columns'
+import { COLUMNS } from './columns'
 import './table.css'
 
 const ReactTablePage = () => {
-    const columns = useMemo(() => GROUPED_COLUMNS, []) // This ensures that the columns are not re-created on every render
+    const columns = useMemo(() => COLUMNS, []) // This ensures that the columns are not re-created on every render
     const data = useMemo(() => MOCK_DATA, []) // This ensures that the data is not re-created on every render
 
     const tableInstance = useTable({
@@ -17,7 +17,6 @@ const ReactTablePage = () => {
         getTableProps, // This function returns the props that need to be added to the table element
         getTableBodyProps, // This function returns the props that need to be added to the table body element
         headerGroups, // This is an array of header groups. Each header group is an object that contains the headers for that group
-        footerGroups, // This is an array of footer groups. Each footer group is an object that contains the footers for that group
         rows, // This is an array of rows. Each row is an object that contains the cells for that row
         prepareRow // This function needs to be called on each row before getting the row props
     } = tableInstance
@@ -55,19 +54,6 @@ const ReactTablePage = () => {
                         })
                     }
                 </tbody>
-                <tfoot>
-                    {
-                        footerGroups.map(footerGroup => (
-                            <tr {...footerGroup.getFooterGroupProps()}>
-                                {
-                                    footerGroup.headers.map(column => (
-                                        <td {...column.getFooterProps()}>{column.render('Footer')}</td>
-                                    ))
-                                }
-                            </tr>
-                        ))
-                    }
-                </tfoot>
             </table>
         </div>
     )
